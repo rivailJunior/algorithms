755075bbd4a21f877909c95e70449eaf5a8fa311
rivailJunior <rivail.rj@gmail.com>
2023-11-24 20:53:34 -0400

    reverse

---
+++ reverse/reverse.js
@@ -0,0 +1,14 @@
+function reverse(str) {
+  let reverseAux = [];
+  let reverseResult = "";
+  for (let a = 0; a < str.length; a++) {
+    reverseAux.push(str[a]);
+  }
+  for (let a = reverseAux.length - 1; a >= 0; a--) {
+    reverseResult += str[a];
+  }
+
+  return reverseResult;
+}
+
+reverse("apple");
