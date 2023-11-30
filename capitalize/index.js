6e45121dc3207e0d6ba24192f2ed8ff668bc647d
rivail.santos <rivail.santos@olist.com>
2023-11-30 16:11:34 -0400

    capitalize

---
+++ capitalize/index.js
@@ -0,0 +1,19 @@
+/*
+given a string as parameter, capitalize the string and return the capitalized string
+ex: i'm playing soccer -> I'm Playing Soccer
+*/
+function capitalize(str) {
+  const arr = str.split(" ");
+  let newStr = "";
+  for (const key of arr) {
+    const sliced = key.slice(1);
+    newStr += key[0].toUpperCase() + sliced + " ";
+  }
+  return newStr;
+}
+
+capitalize("hello i'm rivail santos");
+
+capitalize("hi there, i'm doing the mac instalation");
+
+module.exports = capitalize;
